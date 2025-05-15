dfcabc1ec5f11bda48f5931cf04120718d0fc4b5
Michael (Micky) Lephend <idpwf1@gmail.com>
2025-05-15 14:13:56 -0500

    Fix multiple install
    Improved comments, logging

--- background.js
+++ background.js
@@ -2,21 +2,43 @@ const EXTENSION_ID = "15E660EF-2CB3-411F-BCB6-8F414FDFC28A"
 
 defaultTabs = {}
 
-// TODO
-chrome.contextMenus.create({
-    title: 'Default tab for this window',
-    contexts: ['all'],
-    id: EXTENSION_ID
+//Installing context menu item
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.contextMenus.create({
+        title: 'Make default tab',
+        contexts: ['all'],
+        id: EXTENSION_ID
+    });    
 });
 
-//install event handler
+//Installing event handler
 chrome.contextMenus.onClicked.addListener(onClickHandler);
 
-function onClickHandler(clickInfo) {
-    console.log("Received clickInfo: " + clickInfo);
+function onClickHandler(clickInfo, clickTabInfo) {
+    console.log('Executing onClickHandler');
+    if (!clickTabInfo.id || clickTabInfo.id == chrome.tabs.TAB_ID_NONE) {
+        console.error('Tab id not found or invalid - aborting!');
+        return;
+    }
+
+    if (defaultTabs[clickTabInfo.windowId]) {
+        console.log('Current default tab for window %d is %d', 
+            clickTabInfo.windowId,
+            defaultTabs[clickTabInfo.windowId]
+        ); //TODO REMOVE DEBUG !!!
+    } else {
+        console.log('Current default tab for window %d has not been set', 
+            clickTabInfo.windowId
+        );
+    }
+
+    defaultTabs[clickTabInfo.windowId] = clickTabInfo.id;
 }
 
+
 //handle tab close
+
+
 //handle tab move between windows
 
 
