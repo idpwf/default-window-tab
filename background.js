ab0082c965b2cc3e165e4b8973b158e19571fb5d
Michael (Micky) Lephend <idpwf1@gmail.com>
2025-05-17 02:41:16 -0500

    `setStorage` seems to work in principle but I'm too tired to be really sure or understand _why_ it does work

--- background.js
+++ background.js
@@ -1,20 +1,37 @@
 //TODO move to util
-async function getFromStorage(windowId) {
-    console.warn('get storage :: Before promise2 created ; windowId is ' + windowId);
+async function getStorage(key) {
+    console.warn('get storage :: Before promise2 created ; key is ' + key);
 
-    var promise2 = chrome.storage.local.get(windowId.toString());
+    return chrome.storage.local.get(key.toString());
+}
 
-    console.warn('get storage :: After promise2 created');
+async function setStorage(kv) {
+    // k = key.toString();
+    // console.log('key is %s ; k is %s ; are they equal? : ', key, k, key == k);
+    // console.log('value is %s ', value);
+    // kv = {k : value};
+    // console.log('kv is %O', kv);
 
-    promise2.then((result) => {
-        console.warn('get storage :: promise2 then2: ' + result); 
-        new Promise(function(resolve) {
-            console.warn('will resolve the inner promise with result ' + result)
-            resolve(result);
-        });
-    });
+    return chrome.storage.local.set(kv);
+    // console.log('p1 is %O', p1);
+    
+    // var promise2 = chrome.storage.local.set({k: value});
 
-    return await promise2;
+    // console.warn('set storage :: After promise2 created ; setting %s to %s' , key, value);
+
+    // promise2.then((result) => {
+    //     console.warn('set storage :: promise2 then2 ; result is %s', result); 
+    //     new Promise(function(resolve) {
+    //         console.warn('set storage ::  will resolve the inner promise  ');
+    //         resolve();
+    //     });
+    // });
+
+    // result2 = await promise2;
+
+    // console.warn('awaited and received result %s', result2);
+
+    // return await result2;
 }
 
 const EXTENSION_ID = '15E660EF-2CB3-411F-BCB6-8F414FDFC28A'
@@ -29,17 +46,19 @@ chrome.runtime.onInstalled.addListener(() => {
         id: EXTENSION_ID
     });
 
-    console.warn('Before promise1')
+    console.warn('before setting x, y')
 
-    var promise1 = chrome.storage.local.set({'x': 42});
+    chrome.storage.local.set({'x': 7});
+    setStorage({'y': 8});
+    setStorage({11: 12});
 
-    console.warn('After promise1 created');
+    console.warn('After setting x, y');
 
-    promise1.then(() => { console.warn('after promise1 is fulfilled'); 
-        new Promise(function(resolve) {
-            resolve();
-        });
-    });
+    // promise1.then(() => { console.warn('after promise1 is fulfilled'); 
+    //     new Promise(function(resolve) {
+    //         resolve();
+    //     });
+    // });
 });
 
 
@@ -56,19 +75,30 @@ async function onContextMenuClickHandler(clickInfo, clickTabInfo) {
 
 
     console.log('will get x from storage');
+    x = await getStorage('x');
+    console.log('the value of x is %s', x['x']);
 
-    x = await getFromStorage('x');
+    console.log('will get y from storage');
+    y = await getStorage('y');
+    console.log('the value of y is %s', y['y']);
 
-    console.log('type of x is %s and the value of x is %O', typeof(x), x);
+    console.log('will get z from storage');
+    z = await getStorage('z');
+    console.log('the value of z is %s', z['z']);
+
+    console.log('will get 11 from storage');
+    eleven = await getStorage(11);
+    console.log('the value of 11 is %s', eleven[11]);
 
     var w = clickTabInfo.windowId.toString();
     var t = clickTabInfo.id;
+
 
     // chrome.storage.local.set({w: t}).then(() => {
     //     console.info('Done setting tab %d as default tab for window %d', clickTabInfo.id, clickTabInfo.windowId);
     // });
 
-    console.info('Will set tab %d as default tab for window %d', clickTabInfo.id, clickTabInfo.windowId);
+    // console.info('Will set tab %d as default tab for window %d', clickTabInfo.id, clickTabInfo.windowId);
     // defaultTabs[clickTabInfo.windowId] = clickTabInfo.id;
 }
 
@@ -76,7 +106,7 @@ async function onContextMenuClickHandler(clickInfo, clickTabInfo) {
 //Installing window onBoundsChanged handler
 chrome.windows.onBoundsChanged.addListener(onBoundsChangedHandler);
 
-function onBoundsChangedHandler(windowInfo) {
+async function onBoundsChangedHandler(windowInfo) {
     console.debug('Executing onBoundsChangedHandler');
 
     if (windowInfo.state == 'minimized') {
@@ -98,28 +128,28 @@ function onBoundsChangedHandler(windowInfo) {
 //Installing tab onRemoved (tab closed) handler
 chrome.tabs.onRemoved.addListener(onRemovedHandler);
 
-function onRemovedHandler(tabId, removeInfo) {
+async function onRemovedHandler(tabId, removeInfo) {
     console.debug('Executing onRemovedHandler');
 
     console.debug('onRemovedHandler received tab closed event for tab %d : %O', tabId, removeInfo);
 
-    if (defaultTabs[removeInfo.windowId] && defaultTabs[removeInfo.windowId] == tabId) {
-        console.info('Default tab for window %d has been closed - will unset default tab');
-        delete defaultTabs[removeInfo.windowId];
-    }
+    // if (defaultTabs[removeInfo.windowId] && defaultTabs[removeInfo.windowId] == tabId) {
+    //     console.info('Default tab for window %d has been closed - will unset default tab');
+    //     delete defaultTabs[removeInfo.windowId];
+    // }
 }
 
 
 //Installing tab moved between browser windows handler
 chrome.tabs.onDetached.addListener(onDetachedHandler);
 
-function onDetachedHandler(tabId, detachInfo) {
+async function onDetachedHandler(tabId, detachInfo) {
     console.debug('Executing onDetachedHandler');
 
     console.debug('onDetachedHandler received tab detached event for tab %d : %O', tabId, detachInfo);
 
-    if (defaultTabs[detachInfo.oldWindowId] && defaultTabs[detachInfo.oldWindowId] == tabId) {
-        console.info('Default tab %d has been detached from its window %d - will unset default tab', tabId, detachInfo.oldWindowId)
-        delete defaultTabs[detachInfo.oldWindowId];
-    }
+    // if (defaultTabs[detachInfo.oldWindowId] && defaultTabs[detachInfo.oldWindowId] == tabId) {
+    //     console.info('Default tab %d has been detached from its window %d - will unset default tab', tabId, detachInfo.oldWindowId)
+    //     delete defaultTabs[detachInfo.oldWindowId];
+    // }
 }
